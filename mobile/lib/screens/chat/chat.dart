cec6fb60f921bc3e4db6eeaf2a9b9ef2fb8c38e3
Kawthar Shaalan <101220311@mu.edu.lb>
2026-01-21 00:39:15 +0200

    rebuilt the chat with screen reader tags

--- mobile/lib/screens/chat/chat.dart
+++ mobile/lib/screens/chat/chat.dart
@@ -1,176 +1,53 @@
-import 'dart:math' as math;
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
+import 'package:flutter/services.dart';
 import 'package:mobile/constants/colors.dart';
 import 'package:mobile/constants/paddings.dart';
 
-class VoiceChatPage extends StatefulWidget {
-  const VoiceChatPage({Key? key}) : super(key: key);
+class VoiceChatPage extends StatefulWidget{
+    const voiceChatPage({Key? key}) : super(key:key);
 
-  @override
-  State<VoiceChatPage> createState() => VoiceChatState();
+    @override
+    State<VoiceChatPage> createState() => VoiceChatState();
 }
-
-class VoiceChatState extends State<VoiceChatPage>
+class VoiceChatState extends State<VoiceChatPage> 
     with TickerProviderStateMixin {
+        late AnimationController pulseController;
+        late AnimationController waveController;
+        bool isRecording = false;
 
-  late AnimationController pulseController;
-  late AnimationController waveController;
-  bool isRecording = false;
+        @override
+        void initState(){
+            super.initState();
 
-  @override
-  void initState() {
-    super.initState();
+            pulseController = AnimationController(
+                duration: const Duration(milliseconds:2000),
+                vsync: this,
+            )..repeat();
 
-    pulseController = AnimationController(
-      duration: const Duration(milliseconds: 2000),
-      vsync: this,
-    )..repeat();
+            waveController = AnimationController(
+                duration:const Duration(milliseconds: 1500),
+                vsync:this,
+            )..repeat();
+        }
 
-    waveController = AnimationController(
-      duration: const Duration(milliseconds: 1500),
-      vsync: this,
-    )..repeat();
-  }
+        @override
+        void dispose(){
+            pulseController.dispose();
+            waveController.dispose();
+            super.dispose();
+        }
 
-  @override
-  void dispose() {
-    pulseController.dispose();
-    waveController.dispose();
-    super.dispose();
-  }
+        Future<void> toggleRecording() async {
+            setState((){
+                isRecording = !isRecording;
+            });
+            String announcement = isRecording ? 'Listening Started' : 'Recording Stopped';
+            await SemanticsService.announce(
+                announcement,
+                TextDirection.ltr,
+            );
+        }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.arrow_back, color: Colors.black87),
-                    onPressed: () => Navigator.pop(context),
-                  ),
-                  const Text(
-                    'Voicera Voice Chat',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black87,
-                    ),
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.more_vert, color: Colors.black87),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-            ),
-
-            Expanded(
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-
-                    Stack(
-                      alignment: Alignment.center,
-                      children: [
-
-                        AnimatedBuilder(
-                          animation: waveController,
-                          builder: (context, child) {
-                            return Container(
-                              width: 220,
-                              height: 220,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border: Border.all(
-                                  color: AppColors.teal.withOpacity(
-                                    isRecording
-                                        ? 0.2 * (1 - waveController.value)
-                                        : 0.05,
-                                  ),
-                                  width: 18,
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-
-                        GestureDetector(
-                          onTap: () {
-                            setState(() => isRecording = !isRecording);
-                          },
-                          child: Container(
-                            width: 180,
-                            height: 180,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              gradient: LinearGradient(
-                                begin: Alignment.topLeft,
-                                end: Alignment.bottomRight,
-                                colors: [
-                                  AppColors.teal,
-                                  AppColors.teal.withOpacity(0.7),
-                                ],
-                              ),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: AppColors.teal.withOpacity(0.3),
-                                  blurRadius: 20,
-                                  spreadRadius: 5,
-                                ),
-                              ],
-                            ),
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  SizedBox(height: AppPadding.vP),
-                                  Text(
-                                    isRecording
-                                        ? 'Listening...'
-                                        : 'Tap to speak',
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    const SizedBox(height: 40),
-
-                    Text(
-                      isRecording
-                          ? 'Speak clearly into the microphone'
-                          : 'Press the circle and speak',
-                      style: const TextStyle(
-                        color: Colors.black54,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+        
+    }
