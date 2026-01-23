372957b26f9f9eec88dee399feabfd2313e41669
Kawthar Shaalan <101220311@mu.edu.lb>
2026-01-23 08:38:04 +0200

    cleaned the mobile

--- mobile/lib/screens/chat/services/agent_service.dart
+++ mobile/lib/screens/chat/services/agent_service.dart
@@ -4,7 +4,6 @@ import 'package:mobile/apis/auth_service.dart';
 
 class AgentService {
   final String baseUrl;
-
   AgentService(this.baseUrl);
   Future<String> ask(String question) async {
     final response = await http
@@ -12,9 +11,7 @@ class AgentService {
           Uri.parse('$baseUrl/api/ask-anything'),
           headers: AuthService.headers,
           body: jsonEncode({'question': question}),
-        )
-        .timeout(const Duration(seconds: 30));
-
+        );
     if (response.statusCode != 200) {
       throw Exception('Agent Error (${response.statusCode}): ${response.body}');
     }
