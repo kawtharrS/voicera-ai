ce1a76877d22b62eb5471b39e88f0f5d59398b5f
Kawthar Shaalan <101220311@mu.edu.lb>
2026-01-26 06:37:43 +0200

    fixed docker

--- backend/common/health_handler.go
+++ backend/common/health_handler.go
@@ -5,14 +5,9 @@ import (
 	"net/http"
 )
 
-func FastAPIHealthHandler(w http.ResponseWriter, r *http.Request){
-	health, err := GetFastAPIHealth()
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadGateway)
-		return
-	}
-
+func FastAPIHealthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(health)
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "hi",
+	})
 }
